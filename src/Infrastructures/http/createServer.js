14598bb14251b32ad4a2f0766b1c0383e8640874
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-08-22 17:49:25 +0700

    Selamat datang

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -68,7 +68,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      value: 'Hello world!'
+      value: 'Selamat datang di Forum API!'
     })
   })
 
